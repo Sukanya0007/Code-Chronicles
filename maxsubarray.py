a7aea4feab640950b104c660aaa20287bde99a01
Sukanya0007 <sukanyabanik80@gmail.com>
2026-09-12 22:11:09 +0530

    Create maxsubarray.py

---
+++ maxsubarray.py
@@ -0,0 +1,12 @@
+# Problem: Maximum Subarray (#53)
+# Difficulty: Easy
+# Date: September 12, 2026
+
+class Solution:
+    def maxSubArray(self, nums):
+        max_sum = nums[0]
+        current_sum = nums[0]
+        for num in nums[1:]:
+            current_sum = max(num, current_sum + num)
+            max_sum = max(max_sum, current_sum)
+        return max_sum
