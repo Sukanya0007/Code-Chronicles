9db4bf4bd08d971b55853b68184b751036b3b66c
Sukanya0007 <sukanyabanik80@gmail.com>
2026-09-13 10:29:03 +0530

    Create validPaindrome.py

---
+++ validPaindrome.py
@@ -0,0 +1,9 @@
+# Problem: Valid Palindrome (#125)
+# Difficulty: Easy
+# Date: September 13, 2026
+# Topic: Strings, Two Pointers
+
+class Solution:
+    def isPalindrome(self, s):
+        cleaned = ''.join(c.lower() for c in s if c.isalnum())
+        return cleaned == cleaned[::-1]
